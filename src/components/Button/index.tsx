443863a04941f991c43a71fe2feef80deafc2d40
Sander van Rijsoort <hallo@svanrijsoort.nl>
2021-01-27 18:29:39 +0000

    added button components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+
+import './button.scss';
+
+interface ButtonProps {
+  children: string;
+  isPrimary: boolean;
+  clickHandler: () => void;
+}
+
+export const Button: FC<ButtonProps> = ({
+  children,
+  isPrimary,
+  clickHandler
+}) => {
+  const primaryStyle = isPrimary && `btn--primary`;
+  const classes = `btn ${primaryStyle}`;
+  return (
+    <button className={classes} type="submit" onClick={clickHandler}>
+      {children}
+    </button>
+  );
+};
